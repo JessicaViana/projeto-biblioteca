3b5b305cf891f4c1f252f25f63d36d67dfedac9e
Jessica Viana <v.jessi.carvalho@gmail.com>
2021-10-24 10:27:28 -0300

    corrigido filtros em listagem de empréstimo

--- Biblioteca/Models/EmprestimoService.cs
+++ Biblioteca/Models/EmprestimoService.cs
@@ -34,7 +34,37 @@ namespace Biblioteca.Models
         {
             using(BibliotecaContext bc = new BibliotecaContext())
             {
-                return bc.Emprestimos.Include(e => e.Livro).ToList();
+                //return bc.Emprestimos.Include(e => e.Livro).ToList();
+                IQueryable<Emprestimo> query;
+                
+                if(filtro != null)
+                {
+                    //definindo dinamicamente a filtragem
+                    switch(filtro.TipoFiltro)
+                    {
+                        case "Usuario":
+                            query = bc.Emprestimos.Where(e => e.NomeUsuario.Contains(filtro.Filtro));
+                        break;
+
+                        case "Livro":
+                            query = bc.Emprestimos.Where(e => (bc.Livros.Where(l => l.Titulo.Contains(filtro.Filtro)).Select(l => l.Id)).Contains(e.LivroId) );
+                        break;
+
+                        default:
+
+                            query = bc.Emprestimos;
+                        break;
+                    }
+                }
+                else
+                {
+                    // caso filtro não tenha sido informado
+                    query = bc.Emprestimos;
+                }
+                
+                //ordenação padrão
+                return query.Include(e => e.Livro).ToList();
+            
             }
         }
 
